fc3c1988ef810dbcf754dca9a6f258d8bb26aee1
virmerson <virmerson@hightechcursos.com.br>
2015-11-24 21:16:21 -0300

    Clicando na Item da List

--- app/src/main/java/br/com/fabricadeprogramador/fdpapp/agenda/AgendaListaActivity.java
+++ app/src/main/java/br/com/fabricadeprogramador/fdpapp/agenda/AgendaListaActivity.java
@@ -6,8 +6,11 @@ import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import br.com.fabricadeprogramador.fdpapp.R;
 import br.com.fabricadeprogramador.fdpapp.agenda.adapter.PessoaListAdapter;
@@ -29,6 +32,13 @@ public class AgendaListaActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.agenda_lista);
         ButterKnife.bind(this);
+
+        lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(AgendaListaActivity.this, "Clicou no item!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
